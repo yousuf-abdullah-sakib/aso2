beaa82a14157894783ed73bb91b83fab2d9649eb
Yousuf Abdullah Sakib <141171865+yousuf-abdullah-sakib@users.noreply.github.com>
2023-11-16 02:21:44 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,191 @@
+(function($){
+    'use strict';
+
+
+    $('.all-bid').on('click', function(){
+
+        $('.sure').show();
+
+    });
+   
+   
+
+
+
+
+
+
+    
+    $('.location').on('click', function(){
+
+        $('.loc-area').slideToggle();
+
+    });
+
+    
+    $('.bajed').on('click', function(){
+
+        $('.pop-up-2-b').slideToggle();
+    });
+    $('.pop-up-2-as').on('click', function(){
+
+        $('.pop-up-2-b').slideToggle();
+    });
+
+
+    $('.dtime').on('click', function(){
+
+        $('.pop-up-d').slideToggle();
+    });
+    $('.pop-up-2-c').on('click', function(){
+
+        $('.pop-up-d').slideToggle();
+    });
+
+
+
+    $('.rating').on('click', function(){
+
+        $('.w-pop-up-1').slideToggle();
+    });
+    $('.rating-1').on('click', function(){
+
+        $('.w-pop-up-1').slideToggle();
+    });
+
+
+
+    
+    $('.delivary').on('click', function(){
+
+        $('.w-pop-up-2').slideToggle();
+    });
+    $('.rating-2').on('click', function(){
+
+        $('.w-pop-up-2').slideToggle();
+    });
+
+
+    $('.order-x').on('click', function(){
+
+        $('.w-pop-up-3').slideToggle();
+    });
+    $('.rating-3').on('click', function(){
+
+        $('.w-pop-up-3').slideToggle();
+    });
+    
+
+
+
+
+
+    $('.sb').on('click', function(){
+
+        $('.qn').slideToggle();
+    });
+    $('.sb-1').on('click', function(){
+
+        $('.qn-1').slideToggle();
+    });
+    $('.sb-2').on('click', function(){
+
+        $('.qn-2').slideToggle();
+    });
+
+
+
+
+
+
+
+
+
+
+    $('.complain').on('click', function(){
+
+        $('.pop-12').slideToggle();
+
+    });
+
+    $('.cancel').on('click', function(){
+
+        $('.pop-12').slideToggle();
+
+    });
+
+
+
+
+    $('.menu-icon i').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+    $('.bar-cross i').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+    $('.three-dot-2').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+    $('.s-icon').on('click', function(){
+
+        $('.search').slideToggle();
+
+    });
+
+    $('.s-cross-1 i').on('click', function(){
+
+        $('.search').slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+    $('.db').on('click', function(){
+
+        $('.db-1').slideToggle();
+
+    });
+
+    $('.db-2').on('click', function(){
+
+        $('.db-1').slideToggle();
+
+    });
+
+
+
+
+
+
+
+    
+
+
+
+
+   
+
+
+
+})(jQuery);
